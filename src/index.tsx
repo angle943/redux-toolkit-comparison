97af87649a713006758e38781ecf9e179a92b4a6
Shoaib7889 <shoaib4030891@gmail.com>
2022-04-09 22:53:04 +0500

    added addToCart functionality using react and RTK with TS:

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { Provider } from "react-redux";
-import Store from "./App/store/redux-toolkit";
-import App from "./App";
+import Index from "./ShopingCart/Index";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+// import { Provider } from "react-redux";
+// import Store from "./App/store/redux-toolkit";
+// import App from "./App";
+
+const client = new QueryClient();
 
 ReactDOM.render(
-  <Provider store={Store}>
-    <App />
-  </Provider>,
+  <QueryClientProvider client={client}>
+    <Index />
+  </QueryClientProvider>,
   document.getElementById("root")
 );
